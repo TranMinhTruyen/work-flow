f1251ae5ad429a404d56a777f448d0ad1e4626ba
minh-truyen <badaovodoi9988@gmail.com>
2024-09-23 23:42:29 +0700

    fix not navigate to RegisterPage

--- FE/src/components/layouts/AuthLayout.tsx
+++ FE/src/components/layouts/AuthLayout.tsx
@@ -3,10 +3,25 @@ import Divider from '@mui/material/Divider';
 import Grid2 from '@mui/material/Grid2';
 import authStyles from 'assets/styles/authStyles';
 import { AuthHeaderProvider } from 'common/contexts/AuthHeaderContext';
+import { checkLogin } from 'common/utils/authUtil';
 import AuthHeader from 'components/header/AuthHeader';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const AuthLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isLogin = checkLogin();
+    if (!isLogin) {
+      navigate('/auth/login', { replace: true });
+    } else {
+      navigate('/', { replace: true });
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <AuthHeaderProvider>
       <Grid2 container direction={'column'} sx={authStyles.rootContainer}>
